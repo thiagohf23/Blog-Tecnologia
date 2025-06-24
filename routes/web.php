c8b9bae935837c5a4b7ac7bcabff5feaf87e3756
thiagohf23 <thiiagoft25@gmail.com>
2025-06-24 14:28:09 -0300

    feat(feat): configura tela home/blog

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,21 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 
-Route::get('/',HomeController::class)->name('home');
+Route::get('/', HomeController::class)->name('home');
+
+Route::get('/blog', [PostController::class, 'index'])->name('posts.index');
+
+Route::redirect('/dashboard', '/');
 
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    // Route::get('/dashboard', function () {
+    //     return view('dashboard');
+    // })->name('dashboard');
 });
